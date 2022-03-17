fa868d09eafd4673963c3e106e4e64872590e6a1
Cosmin <cosmin.ciocan@savethechildren.org>
2022-03-17 20:51:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello world!");
+
+        String authorName = "Ionut";
+        byte kgsOfApples = 3;
+
+
+
+        System.out.println(authorName+" a plecat la piata, de pe \n" +
+                "strada Baritiu nr 9, ca sa cumpere "+kgsOfApples+" kg\n" +
+                "de mere. Pe drum, "+    authorName     +" s-a intalnit cu Matilda.\n" +
+                "Impreuna cu Matilda, au mers la piata, iar "+authorName+" a impartit\n" +
+                "cele "+kgsOfApples+" kg de mere cu Matilda.");
+
+
+
+
+        int x = 2;
+        x+=x+=x++ + ++x;
+        System.out.println(x);
+
+
+        double y = 2.4;
+
+        long z = 2L;
+        float a = 2.4F;
+
+
+        int myVariable = x + 3;
+
+
+        double result = x + z;
+        System.out.println(result);
+
+        System.out.println(x + z);
+
+        System.out.println("Valoarea lui x este: " + x);
+        System.out.println(x + z + " este final " + 2);
+
+
+
+
+        System.out.println("Hello world!");
+
+    }
+
+
+}
