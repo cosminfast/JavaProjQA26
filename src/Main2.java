1c801566a15406520a991406aa727732ab8b7370
Cosmin <cosmin.ciocan@savethechildren.org>
2022-03-24 20:06:04 +0200

    lab 2

--- src/Main2.java
+++ src/Main2.java
@@ -1,16 +1,29 @@
 public class Main2 {
+    static int memberVariable = 3;
+
 
     public static void main(String[] args) {
 
-        double result = sum(2, 3);
+        double result = sum(memberVariable, 3.2);
         System.out.println(result * 2);
         System.out.println("Rezultatul este: " + sum(3, 5));
+
+        int result2 = substract(5,4);
+        System.out.println(result2);
+
+        System.out.println(result - result2);
+
+
 
     }
 
     public static double sum(int firstNumber, double secondNumber) {
-        double x = firstNumber + secondNumber;
-        return x;
+        return firstNumber + secondNumber;
+    }
+
+    public static int substract(int firstNumber, int secondNumber){
+        int result = firstNumber - secondNumber;
+        return result;
     }
 
 
