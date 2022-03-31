3d18c5836569278027dbaa61ab4a2749c226389d
Cosmin <cosmin.ciocan@savethechildren.org>
2022-03-31 21:04:02 +0300

    lab 3

---
+++ src/AnimalRescuer/Dog.java
@@ -0,0 +1,29 @@
+package AnimalRescuer;
+
+public class Dog {
+
+    String color = "yellow";
+    double weigth = 20.3;
+    int ageInMonths = 34;
+    String ownerName = "Mirabela";
+    String race = "Labrador";
+    String name = "Constantin";
+    boolean isMale = true;
+
+    public void eat(){
+        System.out.println("Eating dog food");
+    }
+
+    public void sleep(){
+        System.out.println("zzzzZZZ");
+    }
+
+    public void run(){
+        System.out.println("Running ");
+    }
+
+
+    public void play(){
+        System.out.println("playing fetch");
+    }
+}
--- src/calculator/Main3.java
+++ src/calculator/Main3.java
@@ -3,44 +3,25 @@ package calculator;
 public class Main3 {
 
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        LogicalOperations op = new LogicalOperations();
 
 
-//        System.out.println(getMax(12,41));
 
-//        System.out.println(verifyTextFastTrack("FastTrack"));
-
+        System.out.println(op.getMax(12,41));
+        System.out.println(op.verifyTextFastTrack("FastTrack"));
 
         String text = "FastTrack";
-        int number = 6;
+        int number = 2;
+//
 
-        System.out.println(verifyTextAndNumber(text,number));
+        System.out.println(calc.sum(5, 1));
+        System.out.println(calc.substract(5,4));
 
+
+        System.out.println(op.verifyTextAndNumber(text, number));
 
     }
 
-    public static int getMax(int first, int second) {
-        if (first > second) {
-            return first;
-        } else {
-            return second;
-        }
-    }
 
-    public static String verifyTextFastTrack(String text) {
-        if (text.equals("FastTrack")) {
-            return "learning text comparison";
-        } else {
-            return "Got to try some more";
-        }
-    }
-
-    public static String verifyTextAndNumber(String text, int number){
-        if (text.equals("FastTrack") && number <= 3) {
-            return  text + number;
-        } else if (!text.equals("FastTrack") && number >= 4) {
-            return number + text;
-        } else {
-            return "";
-        }
-    }
 }
