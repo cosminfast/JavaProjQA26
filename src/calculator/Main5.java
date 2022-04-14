f3c979196ea27c2508f5fd22c16128338664260e
Cosmin <cosmin.ciocan@savethechildren.org>
2022-04-14 19:31:49 +0300

    lab 5

---
+++ src/calculator/Main5.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        LogicalOperations op = new LogicalOperations();
+        int[] myArray = op.populateArrayToNumber(100);
+
+//        op.printArray(myArray);
+
+        int[] testArray = {12,3,6765,123,6,856};
+        int sum = myArray[0];
+        sum = sum + myArray[1];
+        sum = sum + myArray[2];
+        sum = sum + myArray[3];
+        sum = sum + myArray[4];
+
+        System.out.println(op.getAverageFromArray(myArray));
+        System.out.println(op.getAverageFromArray(testArray));
+    }
+}
